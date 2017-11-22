766356b6a607dfeb1c9bda62bd1a71af0f3e1a80
xiao125 <caixi.tai@qq.com>
2017-11-22 18:27:43 +0800

    配置文件

---
+++ src/main/java/com/imooc/o2o/entity/LocalAuth.java
@@ -0,0 +1,71 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * 本地验证实体类，主要用来做帐号密码的登录
+ */
+public class LocalAuth {
+
+    // 主键ID
+    private Long loaclAuthId;
+    // 帐号
+    private String username;
+    // 密码
+    private  String password;
+    // 创建时间
+    private Date createTime;
+
+    //最近一次的更新时间
+    private Date lastEditTime;
+    // 个人信息，关系为一一对应
+    private PersonInfo personInfo;
+
+    public Long getLoaclAuthId() {
+        return loaclAuthId;
+    }
+
+    public void setLoaclAuthId(Long loaclAuthId) {
+        this.loaclAuthId = loaclAuthId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public PersonInfo getPersonInfo() {
+        return personInfo;
+    }
+
+    public void setPersonInfo(PersonInfo personInfo) {
+        this.personInfo = personInfo;
+    }
+}
