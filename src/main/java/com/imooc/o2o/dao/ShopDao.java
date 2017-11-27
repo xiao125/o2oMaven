0b532c571dd498bd0f2b4aea2fef4dddb082e9ba
xixi <caixi.tai@qq.com>
2017-11-27 23:55:32 +0800

    shop

---
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -0,0 +1,27 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.entity.Shop;
+
+/**
+ * Created by Administrator on 2017/11/27.
+ */
+public interface ShopDao {
+
+    /**
+     * 更新店铺信息
+     * @param shop
+     * @return
+     */
+    int insertShop(Shop shop);
+
+
+    /**
+     * 更新店铺信息
+     * @param shop
+     * @return
+     */
+    int updateShop(Shop shop);
+
+
+
+}
